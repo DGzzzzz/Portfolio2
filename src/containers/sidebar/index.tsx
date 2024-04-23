ce36a3eb6ea77467bc5bcc309288fa38d6048f58
DGzzzzz <douglaswinter75@gmail.com>
2024-04-23 07:26:22 -0300

    finalizado componentes e troca de tema

--- src/containers/sidebar/index.tsx
+++ src/containers/sidebar/index.tsx
@@ -1,9 +1,26 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
 
-const Sidebar = () => (
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
+
+type Props = {
+  trocaTema: () => void
+}
+
+const Sidebar = (props: Props) => (
   <aside>
-    <img src="https://github.com/DGzzzzz.png" />
-    <Titulo fontSize={20}>Douglas Winter</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Douglas Winter</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        DGzzzzz
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Full Stack
+      </Descricao>
+      <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
