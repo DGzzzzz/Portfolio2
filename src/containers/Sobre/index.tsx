946d97058e8448ff3695e7a704e5023bdfba0e55
Douglas Winter <132670815+DGzzzzz@users.noreply.github.com>
2024-07-31 09:28:04 -0300

    Update index.tsx

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -6,10 +6,7 @@ const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre mim</Titulo>
     <Paragrafo>
-      Olá, me chamo Douglas e sou Desenvolvedor Full Stack. Estou todos os dias
-      aprimorando o que já aprendi e buscando novas ferramentas. Cursando ADS no
-      IFRS, progredindo no curso da EBAC e buscando uma oportunidades no mercado
-      de trabalho.
+      Olá! Meu nome é Douglas Winter, e sou um desenvolvedor apaixonado por tecnologia. Atualmente, estou cursando Análise e Desenvolvimento de Sistemas no Instituto Federal do RS e também estou me especializando em Java, com o curso de Desenvolvedor Full Stack Java na EBAC.
     </Paragrafo>
     <GithubSecao>
       <img src="https://github-readme-stats.vercel.app/api?username=DGzzzzz&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
